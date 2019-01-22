b4291efc399613b34330f9161c0b88c93d17c8d2
rauls0t0 <42275360+rauls0t0@users.noreply.github.com>
2019-01-22 09:23:29 -0600

    RuloBB

---
+++ ExampleEquipoMaster/Example_EquipoMaster/Example_EquipoMaster/RuloBB.swift
@@ -0,0 +1,13 @@
+//
+//  RuloBB.swift
+//  Example_EquipoMaster
+//
+//  Created by Univrsidad Politecnica de Gómez Palacio on 22/01/19.
+//  Copyright © 2019 Universidad Politecnica de Gómez Palacio. All rights reserved.
+//
+
+import UIKit
+
+class RuloBB: NSObject {
+
+}
