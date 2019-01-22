e99103097490ee3541761203efe18d744b6cd3d9
rauls0t0 <42275360+rauls0t0@users.noreply.github.com>
2019-01-22 09:06:51 -0600

    Equipo1

---
+++ ExampleEquipoMaster/Example_EquipoMaster/Example_EquipoMaster/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  Example_EquipoMaster
+//
+//  Created by Univrsidad Politecnica de Gómez Palacio on 22/01/19.
+//  Copyright © 2019 Universidad Politecnica de Gómez Palacio. All rights reserved.
+//
+
+import UIKit
+
+class User: NSObject {
+
+}
